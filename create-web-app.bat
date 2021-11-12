7639ac37432d77c507de68db39a9f2007b054cd4
Hanifi YILDIRIMDAĞI <84809402+hanifiyildirimdagi@users.noreply.github.com>
2021-11-13 02:55:39 +0300

    shell file added

---
+++ create-web-app.bat
@@ -0,0 +1,16 @@
+@echo off
+DnsCmd /ZoneAdd %1 /Primary
+DnsCmd /RecordAdd %1 @ A %2
+DnsCmd /RecordAdd %1 www A %2
+DnsCmd /RecordAdd %1 ftp A %2
+dnscmd /recordadd %1 mail A %2
+dnscmd /recordadd %1 @ MX 10 %2
+dnscmd /RecordAdd %1 @ NS ns1.%1
+dnscmd /RecordAdd %1 @ NS ns2.%1
+if not exist "C:\inetpub\www\%1\http" mkdir C:\inetpub\www\%1\http
+if not exist "C:\db\%1\" mkdir C:\db\%1
+cd c:\Windows\System32\inetsrv
+appcmd add site /name:%1 /physicalPath:c:\vhosts\%1 /bindings:http/*:80:%1
+appcmd add apppool /name:%1
+appcmd set site /site.name:%1 /[path='/'].applicationPool:%1
+cd \
